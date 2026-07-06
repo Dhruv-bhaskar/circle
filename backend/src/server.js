ff264edb360d2c473ab49033922a5553fe8307ad
Dhruv-bhaskar <dhruvkumarbhaskar599@gmail.com>
2026-07-06 13:55:55 +0530

    deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -12,7 +12,7 @@ const PORT = ENV.PORT || 3000;
 
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
-app.use(cors({origin: ENV.CLIENT_URL, credentials: true})) //allow cookies from client
+app.use(cors({origin: [ENV.CLIENT_URL, "http://localhost:5173"], credentials: true})) //allow cookies from client
 app.use(cookieParser()) //req.cookies
 
 app.use("/api/auth", authRoutes);
